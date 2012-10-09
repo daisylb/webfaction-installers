4da5d8b03ff77027c9623e5d8717fada62acd178
Adam Brenecki <adam@brenecki.id.au>
2012-10-09 10:36:52 +1030

    Tabs to spaces

--- nginx_uwsgi_python27.py
+++ nginx_uwsgi_python27.py
@@ -46,21 +46,21 @@ http {
 
   # uWSGI upstream config
   upstream wsgi {
-	# Distribute requests to servers based on client IP. This keeps load
-	# balancing fair but consistent per-client. In this instance we're
-	# only using one uWGSI worker anyway.
-	ip_hash;
-	server unix:sock/uwsgi.sock;
+    # Distribute requests to servers based on client IP. This keeps load
+    # balancing fair but consistent per-client. In this instance we're
+    # only using one uWGSI worker anyway.
+    ip_hash;
+    server unix:sock/uwsgi.sock;
   }
 
   server {
-	listen      PORT;
-	charset     utf-8;
+    listen      PORT;
+    charset     utf-8;
 
-	location / {
-	  uwsgi_pass  wsgi;
-	  include     uwsgi_params;
-	}
+    location / {
+      uwsgi_pass  wsgi;
+      include     uwsgi_params;
+    }
   }
 }
 """
@@ -72,67 +72,62 @@ UWSGI_VERSION = '1.3'
 NGINX_VERSION = '1.3.6'
 
 def create(account, app_name, autostart, extra_info, password, server, session_id, username):
-	# create a custom app
-	app = server.create_app(session_id, app_name, 'custom_app_with_port', False, '')
-	
-	install_steps = (
-		# make directories
-		'mkdir bin conf lib lib/python2.7 pid sock app tmp',
-		
-		# download
-		'cd build',
-		'curl -O https://projects.unbit.it/downloads/uwsgi-{}.tar.gz'.format(UWSGI_VERSION),
-		'curl -O http://nginx.org/download/nginx-{}.tar.gz'.format(NGINX_VERSION),
-		
-		# install uwsgi
-		'tar xf uwsgi-{}.tar.gz'.format(UWSGI_VERSION),
-		'cd uwsgi-{}'.format(UWSGI_VERSION),
-		'make',
-		'mv uwsgi ../../bin',
-		'cd ..',
-		
-		# install nginx
-		'tar xf nginx-{}.tar.gz'.format(NGINX_VERSION),
-		'cd nginx-{}'.format(NGINX_VERSION),
-		'./configure',
-		'make',
-		'mv objs/nginx ../../bin',
-		'cd ..',
-		
-		# install required conf files
-		'cd ..',
-		'cp build/nginx-{}/conf/mime.types conf/'.format(NGINX_VERSION),
-		'cp build/uwsgi-{}/nginx/uwsgi_params conf/'.format(UWSGI_VERSION),
-	)
-	
-	# run install steps
-	server.system(session_id, '; '.join(install_steps))
-	
-	# install nginx.conf
-	nginx_conf = NGINX_CONF.replace('APPNAME', app_name).replace('PORT', port)
-	server.write_file(session_id, 'conf/nginx.conf', nginx_conf, 'w')
-	
-	# create database if required
-	if 'postgres' in extra_info:
-		db_name = db_user = '%s_%s' % (username, app_name)
+    # create a custom app
+    app = server.create_app(session_id, app_name, 'custom_app_with_port', False, '')
+    
+    install_steps = (
+        # make directories
+        'mkdir bin conf lib lib/python2.7 pid sock app tmp',
+        
+        # download
+        'cd build',
+        'curl -O https://projects.unbit.it/downloads/uwsgi-{}.tar.gz'.format(UWSGI_VERSION),
+        'curl -O http://nginx.org/download/nginx-{}.tar.gz'.format(NGINX_VERSION),
+        
+        # install uwsgi
+        'tar xf uwsgi-{}.tar.gz'.format(UWSGI_VERSION),
+        'cd uwsgi-{}'.format(UWSGI_VERSION),
+        'make',
+        'mv uwsgi ../../bin',
+        'cd ..',
+        
+        # install nginx
+        'tar xf nginx-{}.tar.gz'.format(NGINX_VERSION),
+        'cd nginx-{}'.format(NGINX_VERSION),
+        './configure',
+        'make',
+        'mv objs/nginx ../../bin',
+        'cd ..',
+        
+        # install required conf files
+        'cd ..',
+        'cp build/nginx-{}/conf/mime.types conf/'.format(NGINX_VERSION),
+        'cp build/uwsgi-{}/nginx/uwsgi_params conf/'.format(UWSGI_VERSION),
+    )
+    
+    # run install steps
+    server.system(session_id, '; '.join(install_steps))
+    
+    # install nginx.conf
+    nginx_conf = NGINX_CONF.replace('APPNAME', app_name).replace('PORT', port)
+    server.write_file(session_id, 'conf/nginx.conf', nginx_conf, 'w')
+    
+    # create database if required
+    if 'postgres' in extra_info:
+        db_name = db_user = '%s_%s' % (username, app_name)
 
 def delete(account, app_name, autostart, extra_info, password, server, session_id, username):
-	# Delete application and database.
-	server.delete_app(session_id, app_name)
-	try:
-		server.delete_db(session_id, '%s_%s' % (username, app_name), 'postgres')
-	except:
-		pass
+    # Delete application and database.
+    server.delete_app(session_id, app_name)
+    try:
+        server.delete_db(session_id, '%s_%s' % (username, app_name), 'postgres')
+    except:
+        pass
 
 if __name__ == '__main__':
-	action, username, password, machine, app_name, autostart, extra_info = sys.argv[1:]
-	server = xmlrpclib.ServerProxy('https://api.webfaction.com/')
-	session_id, account = server.login(username, password, machine)
-	func = locals()[action]
-	try:
-		func(account, app_name, autostart, extra_info, password, server, session_id, username)
-	except Exception as e:
-		from traceback import print_exc
-		print_exc()
-		exit(1)
+    action, username, password, machine, app_name, autostart, extra_info = sys.argv[1:]
+    server = xmlrpclib.ServerProxy('https://api.webfaction.com/')
+    session_id, account = server.login(username, password, machine)
+    func = locals()[action]
+    func(account, app_name, autostart, extra_info, password, server, session_id, username)
 # -----END WEBFACTION INSTALL SCRIPT-----
