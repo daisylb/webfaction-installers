53decd38f5304c86ddaac5d3422120bb2cb27745
Adam Brenecki <adam@brenecki.id.au>
2013-01-20 12:21:11 +1030

    Update nginx-uwsgi-py27 installer to make it class-based

--- webapps/nginx_uwsgi_python27.py
+++ webapps/nginx_uwsgi_python27.py
@@ -16,12 +16,7 @@ When you set up your deployment process, set it up to copy your app to the <appn
 Requires: pip, supervisor
 """
 
-import xmlrpclib
-import sys
-from sys.path import expanduser
-
-_old_stderr = sys.stderr
-sys.stderr = sys.stdout
+from wfinstaller import CustomAppOnPortInstaller
 
 NGINX_CONF = """
 worker_processes  1;
@@ -137,114 +132,86 @@ def application(environ, start_response):
 UWSGI_VERSION = '1.3'
 NGINX_VERSION = '1.3.6'
 
-def create(account, app_name, autostart, extra_info, password, server, session_id, username):
-    # create a custom app
-    # returns a dict containing name, id, machine, autostart, type, port, extra_info
-    app = server.create_app(session_id, app_name, 'custom_app_with_port', False, '')
-    app_dir = "/home/{}/webapps/{}".format(username, app_name)
-    
-    install_steps = (
-        # make directories
-        'mkdir build bin conf lib lib/python2.7 pid sock app tmp',
-        
-        # download
-        'cd build',
-        'mkdir -p ~/.dlcache',
-        'if test ! -f ~/.dlcache/uwsgi-{}.tar.gz'.format(UWSGI_VERSION),
-        'then curl -sSo ~/.dlcache/uwsgi-{v}.tar.gz https://projects.unbit.it/downloads/uwsgi-{v}.tar.gz'.format(v=UWSGI_VERSION),
-        'fi',
-        'cp ~/.dlcache/uwsgi-{}.tar.gz .'.format(UWSGI_VERSION),
-        
-        'if test ! -f ~/.dlcache/nginx-{}.tar.gz'.format(UWSGI_VERSION),
-        'then curl -sSo ~/.dlcache/nginx-{v}.tar.gz http://nginx.org/download/nginx-{v}.tar.gz'.format(v=NGINX_VERSION),
-        'fi',
-        'cp ~/.dlcache/nginx-{}.tar.gz .'.format(NGINX_VERSION),
-        
-        # install uwsgi
-        'tar xf uwsgi-{}.tar.gz'.format(UWSGI_VERSION),
-        'cd uwsgi-{}'.format(UWSGI_VERSION),
-        'python2.7 uwsgiconfig.py --build',
-        'mv uwsgi ../../bin',
-        'cd ..',
-        
-        # install nginx
-        'tar xf nginx-{}.tar.gz'.format(NGINX_VERSION),
-        'cd nginx-{}'.format(NGINX_VERSION),
-        './configure',
-        'make',
-        'mv objs/nginx ../../bin',
-        'cd ..',
-        
-        # install required conf files
-        'cd ..',
-        'cp build/nginx-{}/conf/mime.types conf/'.format(NGINX_VERSION),
-        'cp build/uwsgi-{}/nginx/uwsgi_params conf/'.format(UWSGI_VERSION),
-        'rm -rf build',
-    )
-    
-    # run install steps
-    server.system(session_id, 'bash -ec "{}"'.format('; '.join(install_steps).replace('"', r'\"')))
-    
-    # install nginx.conf and uwsgi.ini
-    nginx_conf = (NGINX_CONF
-        .replace('APPNAME', app_name)
-        .replace('PORT', str(app['port']))
-        .replace('DIR', app_dir)
-        .replace('HOME', '/home/{}'.format(username))
-    )
-    uwsgi_ini = (UWSGI_INI
-        .replace('DIR', app_dir)
-        .replace('APPNAME', app_name)
-        .replace('HOME', '/home/{}'.format(username)))
-    server.write_file(session_id, 'conf/nginx.conf', nginx_conf, 'w')
-    server.write_file(session_id, 'conf/uwsgi.ini', uwsgi_ini, 'w')
-    server.write_file(session_id, 'conf/supervisord.conf', SUPERVISOR_CONFIG.replace('DIR', app_dir).replace('APPNAME', app_name), 'w')
-    server.write_file(session_id, 'reload-app', RELOAD_APP.replace('DIR', app_dir).replace('APPNAME', app_name), 'w')
-    server.write_file(session_id, 'app/wsgi.py', SAMPLE_APP, 'w')
-    
-    # set up app
-    server.system(session_id, 'chmod +x reload-app')
-    server.system(session_id, 'ln -s $PWD/conf/supervisord.conf $HOME/supervisor/app_APPNAME.conf'.replace('APPNAME', app_name))
-    
-    # load app
-    server.system(session_id, '$HOME/bin/supervisorctl update')
-    
-    # create database if required
-    # if 'postgres' in extra_info:
-    #     db_name = db_user = '%s_%s' % (username, app_name)
-    
-    print app['id']
+class NginxUwsgiPython27Installer (CustomAppOnPortInstaller):
 
-def delete(account, app_name, autostart, extra_info, password, server, session_id, username):
-    # Delete application and database.
-    try:
-        server.system('unlink $HOME/supervisor/app_APPNAME.conf'.replace('APPNAME', app_name))
-        server.system('$HOME/bin/supervisorctl update')
-    except:
-        pass
-    
-    server.delete_app(session_id, app_name)
-    
-    # try:
-    #     server.delete_db(session_id, '%s_%s' % (username, app_name), 'postgresql')
-    # except:
-    #     pass
+    def create(self):
+        app_dir = "/home/{}/webapps/{}".format(self.args.username, self.args.app_name)
+        
+        install_steps = (
+            # make directories
+            'mkdir build bin conf lib lib/python2.7 pid sock app tmp',
+            
+            # download
+            'cd build',
+            'mkdir -p ~/.dlcache',
+            'if test ! -f ~/.dlcache/uwsgi-{}.tar.gz'.format(UWSGI_VERSION),
+            'then curl -sSo ~/.dlcache/uwsgi-{v}.tar.gz https://projects.unbit.it/downloads/uwsgi-{v}.tar.gz'.format(v=UWSGI_VERSION),
+            'fi',
+            'cp ~/.dlcache/uwsgi-{}.tar.gz .'.format(UWSGI_VERSION),
+            
+            'if test ! -f ~/.dlcache/nginx-{}.tar.gz'.format(UWSGI_VERSION),
+            'then curl -sSo ~/.dlcache/nginx-{v}.tar.gz http://nginx.org/download/nginx-{v}.tar.gz'.format(v=NGINX_VERSION),
+            'fi',
+            'cp ~/.dlcache/nginx-{}.tar.gz .'.format(NGINX_VERSION),
+            
+            # install uwsgi
+            'tar xf uwsgi-{}.tar.gz'.format(UWSGI_VERSION),
+            'cd uwsgi-{}'.format(UWSGI_VERSION),
+            'python2.7 uwsgiconfig.py --build',
+            'mv uwsgi ../../bin',
+            'cd ..',
+            
+            # install nginx
+            'tar xf nginx-{}.tar.gz'.format(NGINX_VERSION),
+            'cd nginx-{}'.format(NGINX_VERSION),
+            './configure',
+            'make',
+            'mv objs/nginx ../../bin',
+            'cd ..',
+            
+            # install required conf files
+            'cd ..',
+            'cp build/nginx-{}/conf/mime.types conf/'.format(NGINX_VERSION),
+            'cp build/uwsgi-{}/nginx/uwsgi_params conf/'.format(UWSGI_VERSION),
+            'rm -rf build',
+        )
+        
+        # run install steps
+        self.api.system('bash -ec "{}"'.format('; '.join(install_steps).replace('"', r'\"')))
+        
+        # install nginx.conf and uwsgi.ini
+        nginx_conf = (NGINX_CONF
+            .replace('APPNAME', self.args.app_name)
+            .replace('PORT', str(self.port))
+            .replace('DIR', app_dir)
+            .replace('HOME', '/home/{}'.format(self.args.username))
+        )
+        uwsgi_ini = (UWSGI_INI
+            .replace('DIR', app_dir)
+            .replace('APPNAME', self.args.app_name)
+            .replace('HOME', '/home/{}'.format(self.args.username)))
+        self.api.write_file('conf/nginx.conf', nginx_conf, 'w')
+        self.api.write_file('conf/uwsgi.ini', uwsgi_ini, 'w')
+        self.api.write_file('conf/supervisord.conf', SUPERVISOR_CONFIG.replace('DIR', app_dir).replace('APPNAME', self.args.app_name), 'w')
+        self.api.write_file('reload-app', RELOAD_APP.replace('DIR', app_dir).replace('APPNAME', self.args.app_name), 'w')
+        self.api.write_file('app/wsgi.py', SAMPLE_APP, 'w')
+        
+        # set up app
+        self.api.system('chmod +x reload-app')
+        self.api.system('ln -s $PWD/conf/supervisord.conf $HOME/supervisor/app_APPNAME.conf'.replace('APPNAME', self.args.app_name))
+        
+        # load app
+        self.api.system('$HOME/bin/supervisorctl update')
 
-if __name__ == '__main__':
-    try:
-        action, username, password, machine, app_name, autostart, extra_info = sys.argv[1:]
-        server = xmlrpclib.ServerProxy('https://api.webfaction.com/')
-        session_id, account = server.login(username, password, machine)
-        func = locals()[action]
-        func(account, app_name, autostart, extra_info, password, server, session_id, username)
-    except Exception as e:
-        # try to delete the app
+    def delete(self):
+        # Delete application and database.
         try:
-            server.delete_app(session_id, app_name)
+            self.api.system('unlink $HOME/supervisor/app_APPNAME.conf'.replace('APPNAME', app_name))
+            self.api.system('$HOME/bin/supervisorctl update')
         except:
             pass
-        # print the exception
-        from traceback import print_exc
-        with open(expanduser('~/failed_install.log'), 'w') as f:
-            print_exc(f)
+
+if __name__ == '__main__':
+    import sys
+    NginxUwsgiPython27Installer().run(*sys.argv[1:])
 # -----END WEBFACTION INSTALL SCRIPT-----
